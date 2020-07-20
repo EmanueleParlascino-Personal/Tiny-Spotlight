f841727bc03aa25a0d50b6a30ae11dfb349c5773
Emanuele Parlascino <emapage95@gmail.com>
2020-07-20 17:44:05 +0100

    added search for artist functionality

--- src/App.js
+++ src/App.js
@@ -2,36 +2,7 @@ import React, {useState, useEffect}from 'react';
 import Spotify from 'spotify-web-api-js'
 import './App.css';
 
-
-
-// Resources for queries https://developer.spotify.com/documentation/web-api/reference/search/search/
-//https://developer.spotify.com/console/get-search-item/?q=pop&type=artist&market=&limit=&offset=
-// to start server => node auth-server/authorization_code/app.js
-
-/*
-  core functionality pseudo code
-  Search low followers count artists based on genre
-    var offset = 0
-
-    while (smallArtistsPlaylist[].length < minLength || offset > 1999){
-    
-      API call(genre, [artist], {offset: offset, limit: 10} )
-        .then((response) => {
-          for (i =0 ; i <= items.length; i++){
-            if(items(i).followers < maxFollowers && items(i).popularity < maxPopularity && items(i).genre == genre){      
-              smallArtistsPlaylist[].push(item)
-            }
-          }
-        })
-      offset = offset+offset   
-    }
-
-    alternative approach => get recommendations based on seed 
-    spotifyWebAPI.getRecommendations(options)
-*/ 
 const spotifyWebAPI = new Spotify();
-
-
 
 
 function App() {
@@ -52,6 +23,7 @@ function App() {
   const [nowPlaying, setNowPLaying] = useState("Not Checked")
   const [albumImage, setAlbumImage] = useState()
   const [suggestedAlbums, setAlbums] = useState([])
+  const [searchArtist, setArtist] = useState("")
 
   if (isLoggedIn){
     spotifyWebAPI.setAccessToken(params.access_token)
@@ -70,48 +42,30 @@ function App() {
   })
   console.log("this is in the state", suggestedAlbums)
   
+  
+
   function getArtistsArray(){
     var gotAlbums = false
     var queries = 0
     
-    spotifyWebAPI.getRecommendations({seed_artists: "4NHQUGzhtTLFvgF5SZesLK", seed_tracks: "0c6xIDDpzE81m2q797ordA", seed_genres: ["jazz"],max_popularity: 10, limit: 20})
-      .then((response) => {
-        if (suggestedAlbums.length === 0){
-        setAlbums(response.tracks)
-        queries++
-        console.log("this is in the state", suggestedAlbums, queries)
-        gotAlbums = true}
-      })
-    //small change
-    /*
-    var offset = 5
-    const artistsArray = [] 
-    var i 
-    const genre = "jazz"
-    const maxPopularity = 40
-    const maxFollowers = 150
-
-    console.log("started query")
-
-    while( offset < 100){
-      console.log("did one search")
-      spotifyWebAPI.search(genre, ["artist"], {offset: offset, limit:10})
+    if(suggestedAlbums.length == 0){
+      spotifyWebAPI.getRecommendations({seed_artists: "4NHQUGzhtTLFvgF5SZesLK", seed_tracks: "0c6xIDDpzE81m2q797ordA", seed_genres: ["jazz"],max_popularity: 10, limit: 20})
         .then((response) => {
-          var tempArray = response.artists.items
-          for (i = 0; i < tempArray.length; i++ ){
-            if(tempArray[i].popularity <= maxPopularity){
-              artistsArray.push(tempArray[i])
-            }
-          }
+          if (suggestedAlbums.length === 0){
+          setAlbums(response.tracks)
+          queries++
+          console.log("this is in the state", suggestedAlbums, queries)
+          gotAlbums = true}
         })
-        offset += 5
-    }
-    console.log("finished searching")
-    console.log(artistsArray)
-    */
-    
-  }
+      }
+    }  
 
+    function handleSubmit(value){
+      spotifyWebAPI.search(value, ["artist"], {limit:1})
+      .then((response) => {
+        console.log(response)
+      })
+    }
   
   
   return (
@@ -120,14 +74,10 @@ function App() {
       <p className = "statement">In today's musical landscape the competion is ruthless, it's not easy for and indipendent artists to get a chance at recognition and because of that
         there are countless songs that never get the appreciation they deserve. This is a place to try and give these artists some and for the people that are interested in finding them out.
       </p>
-      <form>
+      <form  onSubmit = {handleSubmit()}>
         <label>
           Enter an artist name
-          <input type="text" name="artist" />
-        </label>
-        <label>
-          Or a genre of music
-          <input type="text" name="artist" />
+          <input type="text" name="artist" value = {searchArtist} onChange={e => setArtist(e.target.value)}/>
         </label>
         <input type="submit" value="Submit" />
       </form>
@@ -158,6 +108,64 @@ function App() {
       </div>
     </div>
   );
+
+  
 }
 
+
+
 export default App;
+
+// Resources for queries https://developer.spotify.com/documentation/web-api/reference/search/search/
+//https://developer.spotify.com/console/get-search-item/?q=pop&type=artist&market=&limit=&offset=
+// to start server => node auth-server/authorization_code/app.js
+
+/*
+  core functionality pseudo code
+  Search low followers count artists based on genre
+    var offset = 0
+
+    while (smallArtistsPlaylist[].length < minLength || offset > 1999){
+    
+      API call(genre, [artist], {offset: offset, limit: 10} )
+        .then((response) => {
+          for (i =0 ; i <= items.length; i++){
+            if(items(i).followers < maxFollowers && items(i).popularity < maxPopularity && items(i).genre == genre){      
+              smallArtistsPlaylist[].push(item)
+            }
+          }
+        })
+      offset = offset+offset   
+    }
+
+    alternative approach => get recommendations based on seed 
+    spotifyWebAPI.getRecommendations(options)
+*/ 
+
+//small change
+    /*
+    var offset = 5
+    const artistsArray = [] 
+    var i 
+    const genre = "jazz"
+    const maxPopularity = 40
+    const maxFollowers = 150
+
+    console.log("started query")
+
+    while( offset < 100){
+      console.log("did one search")
+      spotifyWebAPI.search(genre, ["artist"], {offset: offset, limit:10})
+        .then((response) => {
+          var tempArray = response.artists.items
+          for (i = 0; i < tempArray.length; i++ ){
+            if(tempArray[i].popularity <= maxPopularity){
+              artistsArray.push(tempArray[i])
+            }
+          }
+        })
+        offset += 5
+    }
+    console.log("finished searching")
+    console.log(artistsArray)
+    */
